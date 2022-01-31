4ecbf98279b6629fe064e88e17f2c8326c64e530
qaisarh780 <qaisarh780@gmail.com>
2022-01-31 16:38:10 +0500

    Design MainMenu Scene
    Implement UI In Main Menu Scene, Add Privacy Setting Panel , Music btn , Play Btn And Scene Management.

---
+++ Ballerina/Assets/Scripts/SwitchToggle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine ;
+using UnityEngine.UI ;
+
+public class SwitchToggle : MonoBehaviour {
+   [SerializeField] RectTransform uiHandleRectTransform ;
+   Image backgroundImage, handleImage ;
+   Toggle toggle ;
+   Vector2 handlePosition ;
+   void Awake ( ) {
+      toggle = GetComponent <Toggle> ( ) ;
+      handlePosition = uiHandleRectTransform.anchoredPosition ;
+      backgroundImage = uiHandleRectTransform.parent.GetComponent <Image> ( ) ;
+      handleImage = uiHandleRectTransform.GetComponent <Image> ( ) ;
+      toggle.onValueChanged.AddListener (OnSwitch) ;
+
+      if (toggle.isOn)
+         OnSwitch (true) ;
+   }
+
+   void OnSwitch (bool on) {
+      uiHandleRectTransform.anchoredPosition = on ? handlePosition * -1 : handlePosition ; // no anim
+   }
+   void OnDestroy ( ) {
+      toggle.onValueChanged.RemoveListener (OnSwitch) ;
+   }
+}
