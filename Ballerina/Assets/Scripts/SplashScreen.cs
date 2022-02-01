f27f4cb55f1bed2c7a158cf78a5873a51cde4799
qaisarh780 <qaisarh780@gmail.com>
2022-02-01 13:33:43 +0500

    Work On Splash
    Add Loading Screen On Splash Scene.

--- Ballerina/Assets/Scripts/SplashScreen.cs
+++ Ballerina/Assets/Scripts/SplashScreen.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SplashScreen : MonoBehaviour
 {
+    public GameObject loading;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("HELLO");
         StartCoroutine(NextScene());
-        
     }
 
     private IEnumerator NextScene()
     {
-        yield return new WaitForSeconds(6);
-        SceneManager.LoadScene(1);
+        yield return new WaitForSeconds(2);
+        loading.SetActive(true);
+        yield return new WaitForSeconds(2.5f);
+        SceneManager.LoadScene("MainMenu");
     }
 }
